36403fa0082b9022c0e483371dd76b789dd34d1a
Elizabeth Fong <elizabethfongwm@gmail.com>
2013-06-14 01:30:09 -0700

    Documented MainActivity and respective xml file

--- TotemPollProject/TotemPoll/src/main/java/com/grahamp1/polldance/MainActivity.java
+++ TotemPollProject/TotemPoll/src/main/java/com/grahamp1/polldance/MainActivity.java
@@ -1,52 +1,43 @@
-package com.grahamp1.polldance;
+package com.grahamp1.polldance ;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.View;
+import android.app.Activity ;
+import android.content.Intent ;
+import android.os.Bundle ;
+import android.view.View ;
 
-import java.util.ArrayList;
+import java.util.ArrayList ;
 
+
+/**
+ * Activity for the Splash Screen
+ *
+ * @author Android Camp 2013, Graham + 1
+ * @version 14th June 2013
+ */
 public class MainActivity extends Activity
 {
-
+    /**
+     * Initialises the Splash screen.
+     *
+     * @param savedInstanceState The previous saved state of this activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
+
+    /* --- INTENTS TO ACTIVITIES ------------------------------------------------------------------------------------ */
+
+    /**
+     * Starts activity: Shows Host Home Screen
+     *
+     * @param view The view that invokes this method.
+     */
     public void goToHostHome(View view)
     {
         startActivity(new Intent(this, HostHomeActivity.class));
-    }
-
-    public void goToResponseScreen(View view)
-    {
-        // create and start intent
-        Intent intent = new Intent( this , ClientResponseActivity.class ) ;
-        intent.putExtra( "question_object" , generateTestQuestion() ) ;
-
-        startActivity( intent ) ;
-    }
-
-
-    // TEST QUESTION
-    private Question generateTestQuestion()
-    {
-        ArrayList<Answer> tmpList = new ArrayList<Answer>() ;
-
-        Answer tmp = new Answer( "Answer 0" , true ) ;
-        tmpList.add(tmp) ;
-
-        for( int i = 1 ; i < 5 ; i ++ )
-        {
-            tmp = new Answer( "Answer " + i , false ) ;
-            tmpList.add(tmp) ;
-        }
-
-        return new Question( "Test Question" , tmpList ) ;
     }
 }
