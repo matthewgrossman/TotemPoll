7c0a6072b1b312ee1ab3e80ac115f9b9cde70966
Elizabeth Fong <elizabethfongwm@gmail.com>
2013-06-14 02:33:47 -0700

    Host Home activity and display

--- TotemPollProject/TotemPoll/src/main/java/com/grahamp1/polldance/HostHomeActivity.java
+++ TotemPollProject/TotemPoll/src/main/java/com/grahamp1/polldance/HostHomeActivity.java
@@ -5,28 +5,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /**
- * Created by demouser on 6/12/13.
+ * The home activity for the Host.
+ *
+ * @author Google Android Camp 2013, Graham + 1
+ * @version 14th June 2013
  */
-public class HostHomeActivity extends Activity {
-
+public class HostHomeActivity extends Activity
+{
+    /**
+     * Initialises the home activity for the Host.
+     *
+     * @param savedInstanceState The previous saved state of this activity.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_home);
-
     }
 
-    public void goToCreatePoll(View view) {
+
+    /* --- INTENTS FOR OTHER ACTIVITIES ----------------------------------------------------------- */
+
+    /**
+     * Starts the activity for creating a new poll/question.
+     *
+     * @param view The {@code View} object that invokes this method.
+     */
+    public void goToCreatePoll(View view)
+    {
         startActivity(new Intent(HostHomeActivity.this, CreatePollActivity.class));
     }
 
+    /**
+     * Starts the activity for loading a set polls/questions, from a .xml file.
+     *
+     * @param view The {@code View} object that invokes this method.
+     */
     public void goToLoadPoll(View view)
     {
         startActivity( new Intent(this,QuestionSelectionActivity.class) ) ;
